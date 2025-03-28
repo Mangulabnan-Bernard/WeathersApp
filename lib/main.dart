a348732b7b16c9b5672a0a9ad75c78ba8644656b
Bernard Mangulabnan <adaiki287@gmail.com>
2025-03-28 09:27:21 +0800

    tesstt

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'weather_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterPage(),
-    );
-  }
-}
-
-class CounterPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
-      ),
-      body: Center(
-        child: Text(
-          'Counter functionality removed.',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(
+  CupertinoApp(
+    debugShowCheckedModeBanner: false,
+    home: WeatherPage(),
+  ),
+);
