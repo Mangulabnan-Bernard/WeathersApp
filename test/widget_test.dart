c9e2892a00ad0ed0404f7cbbbec9a7ed900557f4
Bernard Mangulabnan <adaiki287@gmail.com>
2025-04-02 16:57:03 +0800

    try

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:weatherapps/weather_page.dart';
+import 'package:weathersapp/weather_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
